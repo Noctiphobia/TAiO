a7428b84a1aced26ce5dc71afcc5af1c1ddc9a61
abdelkarima <a.abdelkarim@student.mini.pw.edu.pl>
2016-10-15 18:52:00 +0200

    Start/stop

--- TAiO/TAiO/ViewModel/MainWindowViewModel.cs
+++ TAiO/TAiO/ViewModel/MainWindowViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using MicroMvvm;
 
 namespace TAiO.ViewModel
 {
@@ -11,5 +13,97 @@ namespace TAiO.ViewModel
 	/// </summary>
 	public class MainWindowViewModel : BaseViewModel
 	{
+		private int _step = 1;
+		private bool _running = false;
+		private bool _stopped = true;
+		private string _playPause = "▶";
+
+
+		/// <summary>
+		/// Krok symulacji.
+		/// </summary>
+		public int Step
+		{
+			get { return _step; }
+			set
+			{
+				_step = value;
+				RaisePropertyChanged(nameof(Step));
+				RaisePropertyChanged(nameof(StepString));
+			}
+		}
+
+		/// <summary>
+		/// Krok symulacji - wersja wpisana przez użytkownika.
+		/// </summary>
+		public string StepString
+		{
+			get { return Step.ToString(); }
+			set
+			{
+				int val;
+				if (int.TryParse(value, out val))
+				{
+					Step = val;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Czy wizualizacja trwa?
+		/// </summary>
+		public bool Running
+		{
+			get { return _running;}
+			set
+			{
+				if (_running != value)
+				{
+					_running = value;
+					PlayPause = Running ? "⏸" : "▶";
+					if (Running)
+						Stopped = false;
+					RaisePropertyChanged(nameof(Running));
+				}
+			}
+		}
+
+		public bool Stopped
+		{
+			get { return _stopped; }
+			set
+			{
+				if (_stopped != value)
+				{
+					_stopped = value;
+					Running = !Stopped;
+					RaisePropertyChanged(nameof(Stopped));
+				}
+			}
+		}
+
+		/// <summary>
+		/// ⏸ lub ▶
+		/// </summary>
+		public string PlayPause
+		{
+			get { return _playPause; }
+			set
+			{
+				_playPause = value;
+				RaisePropertyChanged(nameof(PlayPause));
+			}
+		}
+		
+
+		/// <summary>
+		/// Włącz/wyłącz wizualizację.
+		/// </summary>
+		public ICommand ToggleRunning => new RelayCommand(() =>
+		{
+			Running = !Running;
+		});
+
+		public ICommand Stop => new RelayCommand(() => { Stopped = true; }, () => !Stopped);
 	}
 }
