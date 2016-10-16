090cb56544c2ee487e1f4c9f0c31bfd04e5f5f92
cackoa <cackoa@student>
2016-10-17 00:21:53 +0200

    [Views, Model] added fixes to DP in BoardView, added Array2D class, added some other minor fixes

---
+++ TAiO/TAiO/Model/Array2D.cs
@@ -0,0 +1,14 @@
+﻿namespace TAiO.Model
+{
+	public class Array2D
+	{
+		public Array2D(int[,] array)
+		{
+			Array = array;
+		}
+
+		public int[,] Array { get; set; }
+		public int Width => Array.GetLength(0);
+		public int Height => Array.GetLength(1);
+	}
+}
