4faeab1ff947fbc4e00bc7a1a310eaa53c03bde3
Ahmed Abdelkarim <abdelkarima@student.mini.pw.edu.pl>
2016-11-23 16:01:25 +0100

    Więcej testów blocktype

--- TAiO/TAiO.Tests/BlockTypeTests.cs
+++ TAiO/TAiO.Tests/BlockTypeTests.cs
@@ -6,6 +6,9 @@ namespace TAiO.Tests
     [TestClass]
     public class BlockTypeTests
     {
+        /// <summary>
+        /// Dokładne sprawdzenie standardowych obrotów.
+        /// </summary>
         [TestMethod]
         public void FourRotationsTest()
         {
@@ -22,16 +25,46 @@ namespace TAiO.Tests
                 block.Shape[0][0, 1] == 1 && block.Shape[0][1, 1] == 1;
 
             rotationPassed[1] =
-                block.Shape[1][0, 0] == 0 && block.Shape[1][1, 0] == 1 &&
-                block.Shape[1][0, 1] == 1 && block.Shape[1][1, 1] == 1;
+               block.Shape[1][0, 0] == 1 && block.Shape[1][1, 0] == 1 &&
+               block.Shape[1][0, 1] == 1 && block.Shape[1][1, 1] == 0;
 
             rotationPassed[2] =
-                block.Shape[2][0, 0] == 1 && block.Shape[2][1, 0] == 1 &&
-                block.Shape[2][0, 1] == 1 && block.Shape[2][1, 1] == 0;
+               block.Shape[2][0, 0] == 1 && block.Shape[2][1, 0] == 1 &&
+               block.Shape[2][0, 1] == 0 && block.Shape[2][1, 1] == 1;
 
             rotationPassed[3] =
-                block.Shape[3][0, 0] == 1 && block.Shape[3][1, 0] == 1 &&
-                block.Shape[3][0, 1] == 0 && block.Shape[3][1, 1] == 1;
+                block.Shape[3][0, 0] == 0 && block.Shape[3][1, 0] == 1 &&
+                block.Shape[3][0, 1] == 1 && block.Shape[3][1, 1] == 1;
+
+            for (int i = 0; i < 4; ++i)
+                Assert.IsTrue(rotationPassed[i]);
+        }
+
+        /// <summary>
+        /// Sprawdzenie, czy program wykrył, że klocek jest pionowy i go odwrócił.
+        /// </summary>
+        [TestMethod]
+        public void ChangedToHorizontalTest()
+        {
+            BlockType block = new BlockType(1, 4, new int[1, 4]
+            {
+                {1, 1, 1, 1 }
+            });
+            Assert.AreEqual(1, block.Height);
+            Assert.AreEqual(4, block.Shape[0].Length);
+        }
+
+        /// <summary>
+        /// Sprawdzenie, czy program wykrył powtarzające się obroty.
+        /// </summary>
+        [TestMethod]
+        public void TrimmedRotationsTest()
+        {
+            BlockType block = new BlockType(1, 4, new int[1, 4]
+            {
+                {1, 1, 1, 1 }
+            });
+            Assert.AreEqual(2, block.Shape.Count);
         }
     }
 }
